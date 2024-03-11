0e38c32c66ca29a0e52003426fff8ab10576d13d
hoangvtv <phamtanhoangvtv@gmail.com>
2024-03-11 16:39:21 +0700

    config server to git

---
+++ ConfigServer/src/test/java/com/phamtanhoang/ConfigServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phamtanhoang.ConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
