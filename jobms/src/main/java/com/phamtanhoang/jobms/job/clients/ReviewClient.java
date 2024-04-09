c0b3443f9a9adac7c7da70fb9b9e6137f7a58cc7
hoangvtv <phamtanhoangvtv@gmail.com>
2024-04-09 22:19:44 +0700

    build image services

--- jobms/src/main/java/com/phamtanhoang/jobms/job/clients/ReviewClient.java
+++ jobms/src/main/java/com/phamtanhoang/jobms/job/clients/ReviewClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-@FeignClient(name = "REVIEW-SERVICE")
+@FeignClient(name = "REVIEW-SERVICE",  url = "${review-service.url}")
 public interface ReviewClient {
   @GetMapping("/api/v1/reviews")
   List<Review> getReviews(@RequestParam("companyId") Long companyId);
